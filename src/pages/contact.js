ffcdcf2da9e4ca26e5f5de5ea46a2e311f41c498
Alex Nye <galexynye@gmail.com>
2018-11-14 00:19:04 +0100

    Change mailto temporarily

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -24,9 +24,8 @@ const Contact = () => {
         <h1>Write Us</h1>
         <p>
           If you have any questions or requests we look forward to hearing you!
-          Feel free to{' '}
-          <a href="mailto:contact@musicsequencing.com">email us here</a> or use
-          the contact form below.
+          Feel free to <a href="mailto:galexynye@gmail.com">email us here</a> or
+          use the contact form below.
         </p>
         {/* </p>
         <p>
