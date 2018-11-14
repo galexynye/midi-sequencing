617b631408c33e64f4671e8a9ffedb749893b478
Alex Nye <galexynye@gmail.com>
2018-11-14 21:12:51 +0100

    Fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Wrapper from '../components/Wrapper'
 import Menu from '../components/Menu/Menu'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Wrapper>
-    <Menu sideNav={true} />
+    {/* <Menu sideNav={true} /> */}
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">Click here to go home</Link>
   </Wrapper>
 )
 
