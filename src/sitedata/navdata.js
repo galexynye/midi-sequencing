1c77116161581d7e201a04e20ed27d62cc38f5c9
Alex Nye <galexynye@gmail.com>
2019-02-08 11:52:49 +0100

    Change blog directory to post directory, move gear reviews to post directory, add redirects for all articles that were moved

--- src/sitedata/navdata.js
+++ src/sitedata/navdata.js
@@ -1,6 +1,6 @@
 export const MainNavLinksData = [
     { name: 'Learn', to: '/learn', external: false },
-    { name: 'Blog', to: '/latest', external: false },
+    { name: 'Blog', to: '/blog', external: false },
     { name: 'About', to: '/about', external: false },
     { name: 'Services', to: '/services', external: false },
     { name: 'Forums', to: 'https://forum.musicsequencing.com/', external: true, altStyle: true, }
