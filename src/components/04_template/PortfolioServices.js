7b8d4622b14d2c7f398e5d3b73e6c3838d71c3ac
Alex Nye <galexynye@gmail.com>
2019-02-12 17:39:33 +0100

    Move services portfolio data into the services portfolio for netlify try

--- src/components/04_template/PortfolioServices.js
+++ src/components/04_template/PortfolioServices.js
@@ -3,13 +3,47 @@ import styled from 'styled-components'
 import { findDOMNode } from 'react-dom'
 import ReactPlayer from 'react-player'
 import { ResponsiveIframe, WidthWrapper, GridContainer, FlexboxOrganism, ResponsivePhoto } from '../00_utilities/Utilities';
-import { servicesPortfolio } from "../../sitedata/musicData";
+// import { servicesPortfolio } from "../../sitedata/musicData";
 import { ArticleContainer } from '../05_page/ArticleContainer';
 import { ButtonCTA } from '../01_atom/ButtonCTA';
 import { InputRangeSlider } from '../01_atom/InputRangeSlider';
 import { ButtonNoStyle } from '../01_atom/BottonNoStyle';
 import { msTheme } from '../../styles/Theme';
 import { PortfolioCard } from '../02_molecule/PortfolioCard';
+
+
+// Services Import
+import HangOut from '../../assets/ServicesPortfolio/HangOut_Mastering_v1.mp3'
+import HangOutPic from '../../assets/ServicesPortfolio/Hangout_700px.png'
+import BedOfThorns from '../../assets/ServicesPortfolio/BedOfThorns.mp3'
+import BedOfThornsPic from '../../assets/ServicesPortfolio/BedOfThornsPic.jpg'
+import FartFinale from '../../assets/ServicesPortfolio/FartFinale.mp3'
+import FartFinalePic from '../../assets/ServicesPortfolio/FartFinalePic.jpg'
+import HappyLatin from '../../assets/ServicesPortfolio/HappyLatin_Remixed_v4_AddVoxSamplesBack_256.mp3'
+import HappyLatinPic from '../../assets/ServicesPortfolio/HappyLatin.jpg'
+import Imagine from '../../assets/ServicesPortfolio/Imagine_v8.mp3'
+import ImaginePic from '../../assets/ServicesPortfolio/Imagine_700px.jpg'
+import Karma from '../../assets/ServicesPortfolio/Karma_mp3_256.mp3'
+import KarmaPic from '../../assets/ServicesPortfolio/TSR_Final_WEBpng.jpg'
+import BigEvil from '../../assets/ServicesPortfolio/TheBigEvil.mp3'
+import BigEvilPic from '../../assets/ServicesPortfolio/CinematicSpyGraphics.jpg'
+import RiftPic from '../../assets/ServicesPortfolio/RiftSquare.jpg'
+
+
+
+
+
+export const servicesPortfolio = [
+    { title: "Hang Out", src: HangOut, iframe: false, img: HangOutPic, watchOrListen: "Listen", credits: "Co-writer, Producer, Mixing, Mastering" },
+    { title: "Karma", src: Karma, iframe: false, img: KarmaPic, watchOrListen: "Listen", credits: "Writer, Producer, Mixing" },
+    { title: "Rift", src: "https://www.youtube.com/watch?v=H0Wj1DL-lyg", iframe: true, img: RiftPic, watchOrListen: "Watch", credits: "Writer, Producer, Mixing" },
+    { title: "Imagine", src: Imagine, iframe: false, img: ImaginePic, watchOrListen: "Listen", credits: "Producer, Mixing, Mastering" },
+    { title: "Big Evil", src: BigEvil, iframe: false, img: BigEvilPic, watchOrListen: "Listen", credits: "Writer, Sound Design, Mixing" },
+    { title: "Bed of Thorns", src: BedOfThorns, iframe: false, img: BedOfThornsPic, watchOrListen: "Listen", credits: "Writer, Sound Design, Mixing, Mastering" },
+    { title: "The Finale", src: FartFinale, iframe: false, img: FartFinalePic, watchOrListen: "Listen", credits: "Co-Writer, Music Programming" },
+    { title: "Happy Latin", src: HappyLatin, iframe: false, img: HappyLatinPic, watchOrListen: "Listen", credits: "Writer, Producer, Mixing, Mastering" },
+]
+
 
 
 
