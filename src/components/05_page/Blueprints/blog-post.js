6519e58f900ad4f8666f0f58b4cedabe870e5003
Alex Nye <galexynye@gmail.com>
2019-02-13 16:58:37 +0100

    Try discourse embed on what is midi article with componentDidMount

--- src/components/05_page/Blueprints/blog-post.js
+++ src/components/05_page/Blueprints/blog-post.js
@@ -24,6 +24,23 @@ class BlogPostTemplate extends React.Component {
   //   })();
   // </script>
 
+  componentDidMount() {
+    const postSlug = this.props.data.markdownRemark.fields.slug
+
+    if (postSlug == "/article/what-is-midi") {
+      console.log(postSlug)
+      DiscourseEmbed = {
+        discourseUrl: 'https://forum.musicsequencing.com/',
+        discourseEmbedUrl: `https://www.musicsequencing.com/${postSlug}`
+      };
+
+      (function () {
+        var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
+        d.src = DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
+      })();
+    }
+  }
 
 
   render() {
@@ -142,6 +159,9 @@ export const pageQuery = graphql`
       id
       excerpt
       html
+      fields{
+        slug
+      }
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
